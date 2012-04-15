f6e37a48a404778821b7826208577eff7fc6a6fc
ReluctantX <shadowzxi@live.co.uk>
2012-04-15 20:58:49 +0100

    Linux Core

--- src/scripts/src/ExtraScripts/Setup.h
+++
@@ -1,9 +0,0 @@
-#include "StdAfx.h"
-#ifndef EXTRA_SCRIPTS_SETUP_H
-#define EXTRA_SCRIPTS_SETUP_H
-
-void SetupNeutralGuards(ScriptMgr* mgr);
-void SetupAntiPvP(ScriptMgr * mgr);
-void SetupOnDeath(ScriptMgr * mgr);
-
-#endif
