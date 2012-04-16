3ab43f58f13b6d90ffc99b63fc7fd94687b84758
ReluctantX <shadowzxi@live.co.uk>
2012-04-16 16:22:58 +0100

    Signed-off-by: ReluctantX <shadowzxi@live.co.uk>

--- src/server/worldserver/Master.cpp
+++ src/server/worldserver/Master.cpp
@@ -156,7 +156,7 @@ int Master::Run()
         return 1;
 
     // set server offline (not connectable)
-    LoginDatabase.DirectPExecute("UPDATE realmlist SET color = (color & ~%u) | %u WHERE id = '%d'", REALM_color_OFFLINE, REALM_color_INVALID, realmID);
+    LoginDatabase.DirectPExecute("UPDATE realmlist SET color = (color & ~%u) | %u WHERE id = '%d'", REALM_FLAG_OFFLINE, REALM_FLAG_INVALID, realmID);
 
     ///- Initialize the World
     sWorld->SetInitialWorldSettings();
