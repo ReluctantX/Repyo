72d5b5b3d8b28328f4dfbb4c14b1563b091cd602
ReluctantX <shadowzxi@live.co.uk>
2012-04-18 21:04:04 +0100

    Added Faction/Race/Name change NPC Script... - Uses Vote and Donation Coin's

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -1226,13 +1226,13 @@ void AddBattlegroundScripts()
 
 #ifdef SCRIPTS
 /* This is where custom scripts' loading functions should be declared. */
-
+	void AddSC_NPC_Gossip_Changer();
 #endif
 
 void AddCustomScripts()
 {
 #ifdef SCRIPTS
     /* This is where custom scripts should be added. */
-
+	AddSC_NPC_Gossip_Changer();
 #endif
 }
