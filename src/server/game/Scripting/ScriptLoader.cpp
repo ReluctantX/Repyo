2d6becaf7a5aa2bc0941356214d80e6e1786f3df
ReluctantX <shadowzxi@live.co.uk>
2012-04-19 23:50:25 +0100

    1223

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -1227,7 +1227,6 @@ void AddBattlegroundScripts()
 #ifdef SCRIPTS
 /* This is where custom scripts' loading functions should be declared. */
 	void AddSC_NPC_Gossip_Changer();
-	void AddSC_PvP_System();
 #endif
 
 void AddCustomScripts()
@@ -1235,6 +1234,5 @@ void AddCustomScripts()
 #ifdef SCRIPTS
     /* This is where custom scripts should be added. */
 	AddSC_NPC_Gossip_Changer();
-	AddSC_PvP_System();
 #endif
 }
