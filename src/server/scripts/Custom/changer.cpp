513b9808d041ea13ecbcfb6330493189ae8fbe44
ReluctantX <shadowzxi@live.co.uk>
2012-04-21 18:42:01 +0100

    Trying new way of doing the gossip...

--- src/server/scripts/Custom/changer.cpp
+++ src/server/scripts/Custom/changer.cpp
@@ -38,134 +38,140 @@ class Gossip_Changer : public CreatureScript
                         return true;
                 }
      
-                bool OnGossipSelect(Player * player, Creature * creature, uint32 /*uiSender*/, uint32 uiAction)
+                bool OnGossipSelect(Player * pPlayer, Creature * pCreature, uint32 /*uiSender*/, uint32 uiAction)
                 {
+					pPlayer->PlayerTalkClass->ClearMenus();
+
                         switch(uiAction)
                         {
                                 case 1:
-                                        player->ADD_GOSSIP_ITEM(4, "Buy with Donation Coin's (5) ", GOSSIP_SENDER_MAIN, 2);
-                                        player->ADD_GOSSIP_ITEM(4, "Buy with Vote Coin's (15)", GOSSIP_SENDER_MAIN, 3);
-                                        player->PlayerTalkClass->SendGossipMenu(1, creature->GetGUID());
+									{
+                                        pPlayer->ADD_GOSSIP_ITEM(4, "Buy with Donation Coin's (5) ", GOSSIP_SENDER_MAIN, 2);
+                                        pPlayer->ADD_GOSSIP_ITEM(4, "Buy with Vote Coin's (15)", GOSSIP_SENDER_MAIN, 3);
+                                        pPlayer->SEND_GOSSIP_MENU(1, pCreature->GetGUID());
+									}
                                         break;
      
                                 case 2:
 									{
-                                        if(player->HasItemCount(D_TOKEN, NAME_CHANGE_TOKENS_REQUIRED_D))
+                                        if(pPlayer->HasItemCount(D_TOKEN, NAME_CHANGE_TOKENS_REQUIRED_D))
                                         {
-                                                player->DestroyItemCount(D_TOKEN, NAME_CHANGE_TOKENS_REQUIRED_D, true, false);
-                                                player->SetAtLoginFlag(AT_LOGIN_RENAME);
-                                                CharacterDatabase.PExecute("UPDATE characters SET at_login = at_login | '1' WHERE guid = %u", player->GetGUIDLow());
-												player->GetSession()->SendNotification("Removed x5 [Donation Coin] | Sucess, Please logout to initiate the Name change...");
-                                                player->PlayerTalkClass->SendCloseGossip();
+                                                pPlayer->DestroyItemCount(D_TOKEN, NAME_CHANGE_TOKENS_REQUIRED_D, true, false);
+                                                pPlayer->SetAtLoginFlag(AT_LOGIN_RENAME);
+                                                CharacterDatabase.PExecute("UPDATE characters SET at_login = at_login | '1' WHERE guid = %u", pPlayer->GetGUIDLow());
+												pPlayer->GetSession()->SendNotification("Removed x5 [Donation Coin] | Sucess, Please logout to initiate the Name change...");
+                                                pPlayer->PlayerTalkClass->SendCloseGossip();
      
                                         }
                                         else
                                         {
-                                                player->GetSession()->SendNotification("Failed : You don't have the required Donation Coins! Please purchase more via the Donation shop and try again.");
-                                                player->PlayerTalkClass->SendCloseGossip();
+                                                pPlayer->GetSession()->SendNotification("Failed : You don't have the required Donation Coins! Please purchase more via the Donation shop and try again.");
+                                                pPlayer->PlayerTalkClass->SendCloseGossip();
                                         }
 									}break;
      
                                 case 3:
 									{
-										if(player->HasItemCount(V_TOKEN, NAME_CHANGE_TOKENS_REQUIRED_V))
+										if(pPlayer->HasItemCount(V_TOKEN, NAME_CHANGE_TOKENS_REQUIRED_V))
 										{
-												player->DestroyItemCount(V_TOKEN, NAME_CHANGE_TOKENS_REQUIRED_V, true, false);
-												player->SetAtLoginFlag(AT_LOGIN_RENAME);
-												CharacterDatabase.PExecute("UPDATE characters SET at_login = at_login | '1' WHERE guid = %u", player->GetGUIDLow());
-												player->GetSession()->SendNotification("Removed x15 [Vote Coin] | Sucess, Please logout to initiate the Name change...");
-												player->PlayerTalkClass->SendCloseGossip();
+												pPlayer->DestroyItemCount(V_TOKEN, NAME_CHANGE_TOKENS_REQUIRED_V, true, false);
+												pPlayer->SetAtLoginFlag(AT_LOGIN_RENAME);
+												CharacterDatabase.PExecute("UPDATE characters SET at_login = at_login | '1' WHERE guid = %u", pPlayer->GetGUIDLow());
+												pPlayer->GetSession()->SendNotification("Removed x15 [Vote Coin] | Sucess, Please logout to initiate the Name change...");
+												pPlayer->PlayerTalkClass->SendCloseGossip();
      
 										}
 										else
 										{
-												player->GetSession()->SendNotification("Failed : You don't have the required Vote Coins! Please vote and purchase the coins via the vote store and try again.");
-												player->PlayerTalkClass->SendCloseGossip();
+												pPlayer->GetSession()->SendNotification("Failed : You don't have the required Vote Coins! Please vote and purchase the coins via the vote store and try again.");
+												pPlayer->PlayerTalkClass->SendCloseGossip();
 										}
 									}break;
      
                                 case 4:
-                                player->ADD_GOSSIP_ITEM(4, "Buy with Donation Coin's (10) ", GOSSIP_SENDER_MAIN, 5);
-                                player->ADD_GOSSIP_ITEM(4, "Buy with Vote Coin's (30)", GOSSIP_SENDER_MAIN, 6);
-                                player->PlayerTalkClass->SendGossipMenu(1, creature->GetGUID());
+									{
+                                pPlayer->ADD_GOSSIP_ITEM(4, "Buy with Donation Coin's (10) ", GOSSIP_SENDER_MAIN, 5);
+                                pPlayer->ADD_GOSSIP_ITEM(4, "Buy with Vote Coin's (30)", GOSSIP_SENDER_MAIN, 6);
+                                pPlayer->PlayerTalkClass->SendGossipMenu(1, pCreature->GetGUID());
+									}
                                 break;
      
                                 case 5:
 									{
-                                        if(player->HasItemCount(D_TOKEN, RACE_CHANGE_TOKENS_REQUIRED_D))
+                                        if(pPlayer->HasItemCount(D_TOKEN, RACE_CHANGE_TOKENS_REQUIRED_D))
                                         {
-                                                player->DestroyItemCount(D_TOKEN, RACE_CHANGE_TOKENS_REQUIRED_D, true, false);
-                                                player->SetAtLoginFlag(AT_LOGIN_CHANGE_RACE);
-                                                CharacterDatabase.PExecute("UPDATE characters SET at_login = at_login | '128' WHERE guid = %u", player->GetGUIDLow());
-                                                player->MonsterWhisper("Removed x10 [Donation Coin] | Sucess, Please logout to initiate the Race change...", player->GetGUID());
-                                                player->PlayerTalkClass->SendCloseGossip();
+                                                pPlayer->DestroyItemCount(D_TOKEN, RACE_CHANGE_TOKENS_REQUIRED_D, true, false);
+                                                pPlayer->SetAtLoginFlag(AT_LOGIN_CHANGE_RACE);
+                                                CharacterDatabase.PExecute("UPDATE characters SET at_login = at_login | '128' WHERE guid = %u", pPlayer->GetGUIDLow());
+                                                pPlayer->MonsterWhisper("Removed x10 [Donation Coin] | Sucess, Please logout to initiate the Race change...", pPlayer->GetGUID());
+                                                pPlayer->PlayerTalkClass->SendCloseGossip();
      
                                         }
                                         else
                                         {
-                                                player->GetSession()->SendNotification("Failed : You don't have the required Donation Coins! Please purchase more via the Donation shop and try again.");
-                                                player->PlayerTalkClass->SendCloseGossip();
+                                                pPlayer->GetSession()->SendNotification("Failed : You don't have the required Donation Coins! Please purchase more via the Donation shop and try again.");
+                                                pPlayer->PlayerTalkClass->SendCloseGossip();
                                         }
 									}break;
      
                                 case 6:
 									{
-										if(player->HasItemCount(V_TOKEN, RACE_CHANGE_TOKENS_REQUIRED_V))
+										if(pPlayer->HasItemCount(V_TOKEN, RACE_CHANGE_TOKENS_REQUIRED_V))
 										{
-												player->DestroyItemCount(V_TOKEN, RACE_CHANGE_TOKENS_REQUIRED_V, true, false);
-												player->SetAtLoginFlag(AT_LOGIN_CHANGE_RACE);
-												CharacterDatabase.PExecute("UPDATE characters SET at_login = at_login | '128' WHERE guid = %u", player->GetGUIDLow());
-												player->GetSession()->SendNotification("Removed x30 [Vote Coin] | Sucess, Please logout to initiate the Race change....");
-												player->PlayerTalkClass->SendCloseGossip();
+												pPlayer->DestroyItemCount(V_TOKEN, RACE_CHANGE_TOKENS_REQUIRED_V, true, false);
+												pPlayer->SetAtLoginFlag(AT_LOGIN_CHANGE_RACE);
+												CharacterDatabase.PExecute("UPDATE characters SET at_login = at_login | '128' WHERE guid = %u", pPlayer->GetGUIDLow());
+												pPlayer->GetSession()->SendNotification("Removed x30 [Vote Coin] | Sucess, Please logout to initiate the Race change....");
+												pPlayer->PlayerTalkClass->SendCloseGossip();
      
 										}
 										else
 										{
-												player->GetSession()->SendNotification("Failed : You don't have the required Vote Coins! Please vote and purchase the coins via the vote store and try again.");
-												player->PlayerTalkClass->SendCloseGossip();
+												pPlayer->GetSession()->SendNotification("Failed : You don't have the required Vote Coins! Please vote and purchase the coins via the vote store and try again.");
+												pPlayer->PlayerTalkClass->SendCloseGossip();
 										}
 									}break;
                                         
      
                                 case 7:
-                                player->ADD_GOSSIP_ITEM(4, "Buy with Donation Coin's (20) ", GOSSIP_SENDER_MAIN, 5);
-                                player->ADD_GOSSIP_ITEM(4, "Buy with Vote Coin's (100)", GOSSIP_SENDER_MAIN, 6);
-                                player->PlayerTalkClass->SendGossipMenu(1, creature->GetGUID());
+                                pPlayer->ADD_GOSSIP_ITEM(4, "Buy with Donation Coin's (20) ", GOSSIP_SENDER_MAIN, 5);
+                                pPlayer->ADD_GOSSIP_ITEM(4, "Buy with Vote Coin's (100)", GOSSIP_SENDER_MAIN, 6);
+                                pPlayer->PlayerTalkClass->SendGossipMenu(1, pCreature->GetGUID());
                                 break;
      
                                 case 8:
 									{
-                                        if(player->HasItemCount(D_TOKEN, FACTION_CHANGE_TOKENS_REQUIRED_D))
+                                        if(pPlayer->HasItemCount(D_TOKEN, FACTION_CHANGE_TOKENS_REQUIRED_D))
                                         {
-                                                player->DestroyItemCount(D_TOKEN, FACTION_CHANGE_TOKENS_REQUIRED_D, true, false);
-                                                player->SetAtLoginFlag(AT_LOGIN_CHANGE_FACTION);
-                                                CharacterDatabase.PExecute("UPDATE characters SET at_login = at_login | '64' WHERE guid = %u", player->GetGUIDLow());
-                                                player->GetSession()->SendNotification("Removed x20 [Donation Coin] | Sucess, Please logout to initiate the Faction change...");
-                                                player->PlayerTalkClass->SendCloseGossip();
+                                                pPlayer->DestroyItemCount(D_TOKEN, FACTION_CHANGE_TOKENS_REQUIRED_D, true, false);
+                                                pPlayer->SetAtLoginFlag(AT_LOGIN_CHANGE_FACTION);
+                                                CharacterDatabase.PExecute("UPDATE characters SET at_login = at_login | '64' WHERE guid = %u", pPlayer->GetGUIDLow());
+                                                pPlayer->GetSession()->SendNotification("Removed x20 [Donation Coin] | Sucess, Please logout to initiate the Faction change...");
+                                                pPlayer->PlayerTalkClass->SendCloseGossip();
      
                                         }
                                         else
                                         {
-                                                player->GetSession()->SendNotification("Failed : You don't have the required Donation Coins! Please purchase more via the Donation shop and try again.");
-                                                player->PlayerTalkClass->SendCloseGossip();
+                                                pPlayer->GetSession()->SendNotification("Failed : You don't have the required Donation Coins! Please purchase more via the Donation shop and try again.");
+                                                pPlayer->PlayerTalkClass->SendCloseGossip();
                                         }
 									}break;
      
                                     case 9:
 										{
-											if(player->HasItemCount(V_TOKEN, FACTION_CHANGE_TOKENS_REQUIRED_V))
+											if(pPlayer->HasItemCount(V_TOKEN, FACTION_CHANGE_TOKENS_REQUIRED_V))
 											{
-													player->DestroyItemCount(V_TOKEN, FACTION_CHANGE_TOKENS_REQUIRED_V, true, false);
-													player->SetAtLoginFlag(AT_LOGIN_CHANGE_FACTION);
-													CharacterDatabase.PExecute("UPDATE characters SET at_login = at_login | '64' WHERE guid = %u", player->GetGUIDLow());
-													player->GetSession()->SendNotification("Removed x100 [Vote Coin] | Sucess, Please logout to initiate the Faction change....");
-													player->PlayerTalkClass->SendCloseGossip();
+													pPlayer->DestroyItemCount(V_TOKEN, FACTION_CHANGE_TOKENS_REQUIRED_V, true, false);
+													pPlayer->SetAtLoginFlag(AT_LOGIN_CHANGE_FACTION);
+													CharacterDatabase.PExecute("UPDATE characters SET at_login = at_login | '64' WHERE guid = %u", pPlayer->GetGUIDLow());
+													pPlayer->GetSession()->SendNotification("Removed x100 [Vote Coin] | Sucess, Please logout to initiate the Faction change....");
+													pPlayer->PlayerTalkClass->SendCloseGossip();
      
 											}
 											else
 											{
-													player->GetSession()->SendNotification("Failed : You don't have the required Vote Coins! Please vote and purchase the coins via the vote store and try again.");
-													player->PlayerTalkClass->SendCloseGossip();
+													pPlayer->GetSession()->SendNotification("Failed : You don't have the required Vote Coins! Please vote and purchase the coins via the vote store and try again.");
+													pPlayer->PlayerTalkClass->SendCloseGossip();
 											}
 										}break;
                         }
